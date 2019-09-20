622890d900b10783f1bbbd978867abf8a1e01e69
doghead <861167322@qq.com>
2019-09-20 14:42:13 +0800

    邮箱认证

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,37 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable;
+//    加载使用 MustVerifyEmail trait，打开 vendor/laravel/framework/src/Illuminate/Auth/MustVerifyEmail.php 文件，可以看到以下三个方法：
+//
+//    hasVerifiedEmail() 检测用户 Email 是否已认证；
+//    markEmailAsVerified() 将用户标示为已认证；
+//    sendEmailVerificationNotification() 发送 Email 认证的消息通知，触发邮件的发送。
+//    得益于 PHP 的 trait 功能，User 模型在 use 以后，即可使用以上三个方法。
+    use Notifiable, MustVerifyEmailTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
