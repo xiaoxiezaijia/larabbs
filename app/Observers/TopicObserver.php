94d2c3dcd1ee3c06a4aa429bba10aed51b91e213
doghead <861167322@qq.com>
2019-09-20 17:09:45 +0800

    用户可以创建话题

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -14,6 +14,11 @@ class TopicObserver
         //
     }
 
+    public function saving(Topic $topic)
+    {
+        $topic->excerpt = make_excerpt($topic->body);
+    }
+
     public function updating(Topic $topic)
     {
         //
